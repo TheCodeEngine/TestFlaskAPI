d71537e42472944ce842bdc302fe28e6e5cc437c
Konstantin Stoldt <git@thecodeengine.com>
2014-07-19 12:30:29 +0200

    Foo integrate scaffold structure news

--- api/news/ressources/foo.py
+++ api/news/ressources/foo.py
@@ -1,9 +1,19 @@
 from flask.ext import restful
 
 
-class Foo(restful.Resource):
+class FooList(restful.Resource):
     def get(self):
         pass
 
+
+class FooDetail(restful.Resource):
+    def get(self, id):
+        pass
+
     def post(self):
+        pass
+
+
+class FooActions(restful.Resource):
+    def get(self, id):
         pass
