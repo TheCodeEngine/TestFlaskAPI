c9d55665d29a82b0d1480f3dbda5014671d200d4
Konstantin Stoldt <git@thecodeengine.com>
2014-07-19 12:10:24 +0200

    add requirements.txt

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask.ext.restful import Api, Resource
+
+
+class TaskListAPI(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class TaskAPI(Resource):
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
+
+if __name__ == '__main__':
+    app.run(debug=True)
